20574ce04ec03ac9b7bb71714fa6d29669e5d688
unknown <monishkumarms3@gmail.com>
2025-12-20 20:07:09 +0530

    update

--- tests/public.spec.js
+++ tests/public.spec.js
@@ -39,7 +39,7 @@ test.describe("Public Page Visual Regression", () => {
 			await expect(page).toHaveScreenshot({
 				fullPage: true,
 				animations: "disabled",
-				timeout: 90000, // 🔴 Max wait time
+				timeout: 190000, // 🔴 Max wait time
 			});
 		});
 	}
